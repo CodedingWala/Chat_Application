629f97af577e98173fd7571dc004e47355b55e0e
Javed Moinuddin Ansari <ansarijaved092004@gmail.com>
2025-12-22 20:27:25 +0530

    message route completed

---
+++ backend/src/controller/message.controller.js
@@ -0,0 +1,105 @@
+import cloudinary from "../lib/cloudinary.js"
+import Message from "../models/Message.js"
+import User from "../models/User.js"
+
+export const getAllContacts = async (req, res) => {
+    try {
+        const UserId = req.user._id
+        const contacts = await User.find({ _id: { $ne: UserId } }).select("-password")
+        res.status(200).json(contacts)
+    } catch (error) {
+        console.log("error occured inside the get all contacts route: ", error.message)
+        res.status(500).json({ message: "internel server error" })
+    }
+}
+
+export const getMessages = async (req, res) => {
+    const { id: partnerId } = req.params
+    const myId = req.user._id
+
+    try {
+        const messages = await Message.find(
+            {
+                $or: [
+                    { senderId: myId, receiverId: partnerId },
+                    { senderId: partnerId, receiverId: myId }
+                ]
+            })
+        console.log("message: ", messages)
+        res.status(200).json(messages)
+
+
+    } catch (error) {
+        console.log("error occured inside the get messages route: ", error.message)
+        res.status(500).json({ message: "internel server error" })
+    }
+
+}
+
+export const sendMessages = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params
+        const { text, image } = req.body
+        const senderId = req.user._id
+
+        if (receiverId.equals(sen)) {
+            return res.status(400).json({ message: "you cannot send message to yourself" })
+        }
+        else if (!text && !image) {
+            return res.status(400).json({ message: "text or image is required" })
+        }
+        const user = User.findOne({ _id: receiverId })
+        if (!user) {
+            return res.status(404).json({ message: "user not found" })
+        }
+
+        let imageUrl
+        if (image) {
+            const response = await cloudinary.uploader.upload(image)
+            imageUrl = response.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        const savedMessage = await newMessage.save()
+        res.status(200).json(savedMessage)
+
+        // todo some code here
+
+    } catch (error) {
+        console.log("error occured inside the send messages route: ", error.message)
+        res.status(500).json({ message: "internel server error" })
+    }
+
+}
+
+export const getChatPartners = async (req, res) => {
+    try {
+        const LogedinUserId = req.user._id
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: LogedinUserId },
+                { receiverId: LogedinUserId }
+            ]
+        })
+        const chatPartners = [...new Set(
+            messages.map(message => {
+                return message.senderId.toString() === LogedinUserId.toString()
+                    ? message.receiverId.toString()
+                    : message.senderId.toString()
+            })
+        )] // remove yourself
+        const chatusers = await User.find({ _id: { $in: chatPartners } })
+
+        res.status(200).json(chatusers)
+    } catch (error) {
+        console.log("error occured inside the get chat partners route: ", error.message)
+        res.status(500).json({ message: "internel server error" })
+    }
+}
--- backend/src/middlewre/auth.middleware.js
+++ backend/src/middlewre/auth.middleware.js
@@ -4,16 +4,16 @@ import User from "../models/User.js"
 
 
 
-export const protectRoute = async (req, res) => {
+export const protectRoute = async (req, res,next) => {
     try {
 
-        const token = req.cookies.jwt || req.headers.authorization.split(" ")[1]
+        const token = req.cookies.jwt || req.headers?.authorization?.split(" ")[1]
         if (!token) { return res.status(401).json({ message: "unauthorized" }) }
 
         const decode = jwt.verify(token, ENV.JWT_SECRET)
         if (!decode) { return res.status(401).json({ message: "unauthorized - invalid token" }) }
 
-        const user = await User.findById(decode.useId).select("-password")
+        const user = await User.findOne({_id: decode.userId}).select("-password")
         if (!user) { return res.status(404).json({ message: "unauthorized - user not found" }) }
 
         req.user = user
