5e8c1c06498b6cb9dac737b3de32b730268e7859
Javed Moinuddin Ansari <ansarijaved092004@gmail.com>
2025-12-25 15:42:57 +0530

    chat comntainer completed

---
+++ frontend/src/component/ChatHeader.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { useChatStore } from '../Store/useChatStore'
+import {XIcon} from "lucide-react"
+
+function ChatHeader() {
+    const {SelectedUser, setSelectedUser}=useChatStore()
+    useEffect(() => {
+      const keypress=(e)=>{
+        if(e.key==="Escape"){
+          setSelectedUser(null)
+        }
+      }
+      window.addEventListener("keydown",keypress)
+    
+      return () => {
+        window.removeEventListener("keydown",keypress)
+      }
+    }, [SelectedUser,setSelectedUser])
+    
+      return (
+    <div className='w-full max-h-[100px] flex items-center justify-between bg-slate-500/50 border-b border-slate-700/50 px-4'>
+      <div className='flex justify-center items-center space-x-3 p-4'>
+            <div className='avatar online'>
+                <div className='size-12 rounded-full '>
+                    <img src={`${SelectedUser.profilepic || "/avatar.png"} `}  alt={SelectedUser.fullName} />
+                </div>
+            </div>
+            <div>
+                <h3 className='text-slate-200 font-medium'>{SelectedUser.fullName}</h3>
+                <p className='text-slate-400'>online</p>
+            </div>
+      </div>
+      <button>
+        <XIcon className='w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors' onClick={()=>setSelectedUser(null)}/>
+      </button>
+    </div>
+  )
+}
+
+export default ChatHeader
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -16,8 +16,8 @@ function LoginPage() {
   };
 
   return (
-    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+    <div className="w-full flex items-center justify-center md:p-4 lg:p-4 bg-slate-900">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-screen lg:h-[900px]">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
             {/* FORM CLOUMN - LEFT SIDE */}
